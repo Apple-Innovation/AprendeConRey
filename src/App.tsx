46c548e75312d859eb6c0547da3e0719bd39578a
German <>
2024-07-28 18:27:08 +0200

    Add initial project files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Login } from "./views/Login";
+import "./i18n/config";
+
+function App() {
+  return (
+    <div className="h-dvh w-full flex justify-center items-center bg-blue-300">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
