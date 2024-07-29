56c2589425fd521ed23e19044e050f9ec405a77d
German <>
2024-07-29 23:06:16 +0200

    Update linter rules and add new background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "tile-pattern-light": `url('/AprendeConRey/src/assets/backgroundTile.png?t=1722228234571')`,
+        "tile-pattern-dark": `url('/AprendeConRey/src/assets/darkBackgroundTile.png?t=1722286324108')`,
+      },
+    },
   },
   plugins: [],
 };
