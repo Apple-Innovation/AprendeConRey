2854aacd1bf6846f4175f6636d50678d24dbd27e
German <>
2024-08-15 10:41:02 +0200

    Add xxs font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontSize: {
+        xxs: "0.625rem",
+      },
       backgroundImage: {
         "tile-pattern-light": `url('/src/assets/backgroundTile.png')`,
         "tile-pattern-dark": `url('/src/assets/darkBackgroundTile.png')`,
