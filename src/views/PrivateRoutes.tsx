525f1251bf6f1ca648337a224c756cba965d88e8
German <>
2024-08-15 10:47:57 +0200

    Add PrivateRoutes component

---
+++ src/views/PrivateRoutes.tsx
@@ -0,0 +1,13 @@
+import { useSessionStore } from "@/app/useStore";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+export function PrivateRoutes() {
+  const location = useLocation();
+  const isAuthenticated = useSessionStore((state) => state.user.auth);
+
+  return isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/" state={{ from: location }} />
+  );
+}
