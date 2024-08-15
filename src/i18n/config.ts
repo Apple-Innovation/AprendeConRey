d62ee40b18a0091682e0c0c4af5ebc7bb9b5322d
German <>
2024-08-15 10:42:51 +0200

    Update i18n config: Disable debug mode and comment out en-GB locale import

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import enGB from "./locales/en-GB.json";
+// import enGB from "./locales/en-GB.json";
 import esES from "./locales/es-ES.json";
 
 i18n.on("languageChanged", (lng) => {
@@ -17,13 +17,13 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
+    debug: false,
     // lng: 'en', // we are using a language detector, do not define the lng option
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
     resources: {
-      "en-GB": { translation: enGB },
+      // "en-GB": { translation: enGB },
       "es-ES": { translation: esES },
     },
     fallbackLng: "es-ES",
